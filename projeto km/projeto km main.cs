e07cbe1783610c0800ea587430b3b88b6d594996
USER <USER@USER-PC>
2021-10-17 14:34:27 -0300

    Adicionar arquivos de projeto.

---
+++ projeto km/projeto km main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace projeto_km
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia;
+            double consumo;
+
+            Console.WriteLine("Informe a distancia que sera percorrida");
+            distancia = int.Parse(Console.ReadLine());
+
+            consumo = distancia /10.00;
+
+            Console.WriteLine("A quantidade de combustivel é:{0:N} ", consumo);
+
+        }
+    }
+}
